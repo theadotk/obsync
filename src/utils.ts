7f7d9ee4e1e8b40417ca0369e583de34831383b0
theadotk <131266273+theadotk@users.noreply.github.com>
2026-01-12 00:03:18 +0530

    refactor: use obsidian built-ins for base64

--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,13 @@
-export function base64ToUtf8(base64: string): string {
-    const binaryString = atob(base64);
-    const bytes = new Uint8Array(binaryString.length);
-    for (let i = 0; i < binaryString.length; i++) {
-        bytes[i] = binaryString.charCodeAt(i);
-    }
-    return new TextDecoder().decode(bytes);
+import { arrayBufferToBase64, base64ToArrayBuffer } from "obsidian";
+
+export function base64ToUtf8(base64content: string): string {
+    const buffer = base64ToArrayBuffer(base64content);
+    return new TextDecoder().decode(buffer);
 }
 
 export function utf8ToBase64(text: string): string {
-    const bytes = new TextEncoder().encode(text);
-    let binary = "";
-
-    for (const byte of bytes) {
-        binary += String.fromCharCode(byte);
-    }
-
-    return btoa(binary);
+    const encoder = new TextEncoder();
+    const buffer = encoder.encode(text);
+    return arrayBufferToBase64(buffer);
 }
 
